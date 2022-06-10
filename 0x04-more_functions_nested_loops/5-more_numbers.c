927d1baaadeee4e55d5cd846f66f659e62ca57d2
Ange <ulrichangekoffi2022@gmail.com>
2022-06-09 20:20:32 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that print number for 0 - 9.
+ *
+ *
+ *
+ * Return: void.
+ */
+void more_numbers(void)
+{
+	int i;
+	int j = 0;
+
+	while (j <= 10)
+	{
+	for (i = 0; i <= 14; i++)
+	{
+	if (i > 9)
+	{
+		_putchar((i % 10) + '0');
+	}
+	else
+	{
+		_putchar(i + '0');
+	}
+	}
+		j++;
+		_putchar('\n');
+	}
+}

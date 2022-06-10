c25b7ad965d1040fdd2408270376e73e5ec7c127
Ange <ulrichangekoffi2022@gmail.com>
2022-06-09 20:42:20 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that print numbe.
+ *
+ *@n : int parameter
+ *
+ * Return: void.
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+	for (i = 1; i <= n; i++)
+	{
+	if (i == 1)
+	{
+		_putchar('\\');
+	}
+	else
+	{
+		_putchar('\n');
+	for (j = 1; j < i; j++)
+	{
+		_putchar(' ');
+	}
+		_putchar('\\');
+	}
+	}
+	}
+	_putchar('\n');
+}

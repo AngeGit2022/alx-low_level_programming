52c6ed38c49115c972285124c565dea0200ee624
Ange <ulrichangekoffi2022@gmail.com>
2022-06-13 19:10:26 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strlen - calculate  len of string
+ *
+ * @n : point integer
+ * return: always 0.
+ */
+int _strlen(char *n)
+{
+	return (strlen(n));
+}

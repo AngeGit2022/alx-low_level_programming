f0e14842182a9d1683ac726cac071a7709ae8b61
Ange <ulrichangekoffi2022@gmail.com>
2022-06-14 15:41:24 -0700

    task8

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,21 +1,30 @@
 #include "holberton.h"
 
 /**
- *puts2 - prints every character of a string
+ *puts_half - prints every other character of a string
  *@str: A pointer to an int that will be changed
  *
- * Return: void which means it is correct
+ *Return: void which means our answer is correct
  */
 
-void puts2(char *str)
-{
-int a;
+void puts_half(char *str)
 
-for (a = 0; str[a] != '\0'; a++)
 {
-if (a % 2 == 0)
-	_putchar (str[a]);
-}
+	int i, last;
 
-_putchar ('\n');
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	last = (i + 1) / 2;
+
+	for (i = last; str[i]; i++)
+	{
+		_putchar (str[i]);
+	}
+
+	_putchar ('\n');
+
 }

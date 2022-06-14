0faaeb311e3e72c2f2aac3d727f91f77a9bd46d0
Ange <ulrichangekoffi2022@gmail.com>
2022-06-13 19:56:19 -0700

    task5

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
-int _putchar(char c);
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+void _putchar (char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-

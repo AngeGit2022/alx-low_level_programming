8c17f53c8c00327aec0c8c58840aeb551f468c64
Ange <ulrichangekoffi2022@gmail.com>
2022-06-15 17:54:30 -0700

    task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: a charactere will be changed
+ * @src: a charactere will also changed
+ * return: the resulting of string dest
+ */
+char *_strcat(char *dest, char *src);
+{
+	strcat(dest, src);
+	return (dest);
+}

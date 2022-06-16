36a77dca34b13db8101d7d8ce23a285bbd1f4540
Ange <ulrichangekoffi2022@gmail.com>
2022-06-15 18:13:08 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _strcat - concatenates two strings
+ * _strcat - appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte
  *
  * @dest: a charactere will be changed
  * @src: a charactere will also changed
@@ -10,6 +10,6 @@
  */
 char *_strcat(char *dest, char *src);
 {
-	strcat(dest, src);
+	_strcat(dest, src\n);
 	return (dest);
 }

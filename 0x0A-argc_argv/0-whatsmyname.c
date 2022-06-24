7175dabd5b583d7606d18614b756f93895d95c01
Ange <ulrichangekoffi2022@gmail.com>
2022-06-24 15:46:21 -0700

    argc0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *main - main entry of my program
+ *
+ *@argc : number of argument
+ *
+ *@argv : size of array argument
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

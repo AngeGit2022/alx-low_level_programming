8918d14279b57219c750eb1533585e1877ba550f
Ange <ulrichangekoffi2022@gmail.com>
2022-06-24 16:04:27 -0700

    argc1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - main entry of my program
+ *
+ *@argc : number of argument
+ *
+ *@argv : size of array argument
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}

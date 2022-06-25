4867906b47f898c83db7820ca60aca49d4783cb4
Ange <ulrichangekoffi2022@gmail.com>
2022-06-24 17:22:37 -0700

    argc3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - main entry of program
+ *
+ *@argc : number of argument
+ *
+ *@argv : array of argument
+ *
+ *Return: alway 1 if erro otherwire 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int res;
+
+	if (!(argc > 2))
+	{
+	char error[] = "Error";
+
+	printf("%s\n", error);
+	return (1);
+	}
+	res = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", res);
+	return (0);
+}

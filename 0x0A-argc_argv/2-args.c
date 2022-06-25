3ca9e2a8f6f359f487fc6b5696caea7c9e05bb0d
Ange <ulrichangekoffi2022@gmail.com>
2022-06-24 17:16:18 -0700

    argc2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - main entry of program
+ *
+ *@argc : number of argument
+ *
+ *@argv : array of argument
+ *
+ *Return: alway 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
+
